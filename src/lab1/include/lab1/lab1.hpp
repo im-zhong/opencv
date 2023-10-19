762a59bc3b85ee81766c2b7a8447c88686245afb
zhangzhong <im.zhong@outlook.com>
2023-10-19 23:02:46 +0800

    refactor lab1

--- src/lab1/include/lab1/lab1.hpp
+++
@@ -1,9 +0,0 @@
-// 2023/09/22
-
-#pragma once
-
-#include <string_view>
-
-namespace lab1 {
-auto HelloWorld() -> std::string_view;
-}
--- src/lab1/lab1.cpp
+++ src/lab1/lab1.cpp
@@ -2,23 +2,22 @@
 // zhangzhong
 // https://docs.opencv.org/4.x/db/deb/tutorial_display_image.html
 
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+#include <fstream>
+#include <iostream>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
-
-#include <fstream>
-#include <iostream>
 #include <string>
-
-class MyClass {};
 
 using namespace cv;
 
-void convertToGray(const Mat& img, Mat& gray_img) {
+void ConvertToGray(const Mat& img, Mat& gray_img) {
     // step2: 将图像转成灰度图
     gray_img = Mat(img.rows, img.cols, CV_8UC1);
-    for (int i = 0; i < gray_img.rows; i++) {
-        for (int j = 0; j < gray_img.cols; j++) {
+    for (int row = 0; row < gray_img.rows; row++) {
+        for (int col = 0; col < gray_img.cols; col++) {
             // 通过公式将彩色图像转成灰度图
             // method 1. Average
             // gray_img.at<uchar>(i, j) = (img.at<Vec3b>(i, j)[0] +
@@ -26,27 +25,32 @@ void convertToGray(const Mat& img, Mat& gray_img) {
             // Weighted Average gray = 0.299*r + 0.587*g + 0.114*b
             // 但是实际上opencv的颜色顺序是BGR，所以应该是
             // reference: https://boofcv.org/index.php?title=Example_RGB_to_Gray
-            gray_img.at<uchar>(i, j) = 0.114 * img.at<Vec3b>(i, j)[0] +
-                                       0.587 * img.at<Vec3b>(i, j)[1] +
-                                       0.299 * img.at<Vec3b>(i, j)[2];
+            gray_img.at<uchar>(row, col) = 0.114 * img.at<Vec3b>(row, col)[0] +
+                                           0.587 * img.at<Vec3b>(row, col)[1] +
+                                           0.299 * img.at<Vec3b>(row, col)[2];
         }
     }
 }
 
-int main(int argc, char* argv[]) {
-
-    // get the first arg as path
-    if (argc != 2) {
-        std::printf("Usage: %s <path>\n", argv[0]);
-        return 1;
+void WriteToFile(const Mat& img, const std::string& filename) {
+    std::ofstream fout(filename);
+    for (int row = 0; row < img.rows; row++) {
+        for (int col = 0; col < img.cols; col++) {
+            fout << (int)img.at<uchar>(row, col) << " ";
+        }
+        fout << std::endl;
     }
+    fout.close();
+}
 
-    std::string image_path = samples::findFile(argv[1]);
+TEST_CASE("test lab1") {
+    const char* filename = "/home/zhangzhong/src/opencv/imgs/orange.jpg";
+    std::string image_path = samples::findFile(filename);
     Mat img = imread(image_path, IMREAD_COLOR);
 
     if (img.empty()) {
         std::cout << "Could not read the image: " << image_path << std::endl;
-        return 1;
+        return;
     }
 
     imshow("Display window", img);
@@ -56,7 +60,7 @@ int main(int argc, char* argv[]) {
     // 将图像转成灰度图
     Mat gray_img;
     // cvtColor(img, gray_img, COLOR_BGR2GRAY);
-    convertToGray(img, gray_img);
+    ConvertToGray(img, gray_img);
 
     imshow("Display window", gray_img);
     waitKey(0);
@@ -65,31 +69,22 @@ int main(int argc, char* argv[]) {
     imwrite("imgs/gray_orange.jpg", gray_img);
 
     // step3: 将每一个像素的灰度值写入一个文本文件
-    std::ofstream fout("imgs/gray_orange.txt");
-    for (int i = 0; i < gray_img.rows; i++) {
-        for (int j = 0; j < gray_img.cols; j++) {
-            fout << (int)gray_img.at<uchar>(i, j) << " ";
-        }
-        fout << std::endl;
-    }
-    fout.close();
+    WriteToFile(gray_img, "imgs/gray_orange.txt");
 
     // step 4:
     // 对于每一个像素的灰度值乘以5，再加10，如果得到的值超过255，则将此数值直接用255代替，然后输出该图像
     Mat new_img = gray_img.clone();
-    for (int i = 0; i < new_img.rows; i++) {
-        for (int j = 0; j < new_img.cols; j++) {
+    for (int row = 0; row < new_img.rows; row++) {
+        for (int col = 0; col < new_img.cols; col++) {
             // 防止计算溢出
-            int val = ((int)new_img.at<uchar>(i, j) * 5 + 10) % 256;
+            int val = ((int)new_img.at<uchar>(row, col) * 5 + 10) % 256;
             // if (val > 255) {
             //     val = 255;
             // }
-            new_img.at<uchar>(i, j) = val;
+            new_img.at<uchar>(row, col) = val;
         }
     }
     imshow("Display window", new_img);
     waitKey(0);
     imwrite("imgs/new_gray_orange.jpg", new_img);
-
-    return 0;
 }
